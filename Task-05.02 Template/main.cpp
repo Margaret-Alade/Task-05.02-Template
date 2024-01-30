bc990087c87c8a927a01d7e3eb041b3ecf71dd1f
Margaret Alade <alade.margaret@yandex.ru>
2024-01-30 05:17:14 +0300

    Initial Commit

---
+++ Task-05.02 Template/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task-05.02 Template
+//
+//  Created by Маргарет  on 30.01.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
